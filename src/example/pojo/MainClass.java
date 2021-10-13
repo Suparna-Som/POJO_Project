a568834c3c58c2b53895105dba7c6cb00073c7a9
Suparna Som <92136435+Suparna-Som@users.noreply.github.com>
2021-10-13 15:48:26 +0530

    Updated_MainClass
    added arraylist to object

--- src/example/pojo/MainClass.java
+++ src/example/pojo/MainClass.java
@@ -6,52 +6,64 @@ import java.util.Scanner;
 
     public class MainClass {
         public static void main(String[] args) {
-            Employee emp = new Employee();
+            Employee[] emp = new Employee[3];
             //user Input
+            ArrayList<Object> emp_list = new ArrayList<Object>();
             Scanner sc = new Scanner(System.in);
-            System.out.println("Enter your Name :");
-            String name = sc.nextLine();
 
-            System.out.println("Enter your Designation :");
-            String designation = sc.nextLine();
+            System.out.println("------------------Insert Values------------------");
+                for (int i = 0; i < 3; i++) {
+                    emp[i] = new Employee();
 
-            System.out.println("Enter your Employee_ID :");
-            int id = sc.nextInt();
+                    System.out.println("Enter your Name :");
+                    String name = sc.nextLine();
+                    emp[i].setName(name);
 
-            System.out.println("Enter your Salary :");
-            float salary = sc.nextFloat();
 
-            emp.setName(name);
-            emp.setId(id);
-            emp.setDesignation(designation);
-            emp.setSalary(salary);
+                    System.out.println("Enter your Designation :");
+                    String designation = sc.nextLine();
+                    emp[i].setDesignation(designation);
 
-            System.out.println("------------------Inserted Values------------------");
 
-            System.out.println("Employee Name: " + emp.getName());
-            System.out.println("Employee ID: " + emp.getId());
-            System.out.println("Employee Designation: " + emp.getDesignation());
-            System.out.println("Employee Salary: " + emp.getSalary());
+                    System.out.println("Enter your Employee_ID :");
+                    int id = sc.nextInt();
+                    emp[i].setId(id);
+
+
+                    System.out.println("Enter your Salary :");
+                    float salary = sc.nextFloat();
+                    emp[i].setSalary(salary);
+
+
+                    emp_list.add(emp[i]);
+
+                    System.out.println("Data Inserted Successfully");
+
+
+                    System.out.println("Employee_ID :"+emp[i].getId());
+                    System.out.println("Employee_Name :"+emp[i].getName());
+                    System.out.println("Employee_Designation :"+emp[i].getDesignation());
+                    System.out.println("Employee_Salary :"+emp[i].getSalary());
+                }
 
             System.out.println("------------Representation of ArrayList-------------");
+            System.out.println("");
 
-            ArrayList<Object> emp_list = new ArrayList<Object>();
-            emp_list.add(emp);
             System.out.println(emp_list);
+            System.out.println("");
 
             System.out.println("------------Object list to Json data-------------");
+            System.out.println("");
 
             Gson emp_json=new Gson();
             String ObjTojson =emp_json.toJson(emp);
             System.out.println(ObjTojson);
 
             System.out.println("--------------Json data to Object-----------------");
+            System.out.println("");
 
             Gson emp_obj=new Gson();
-            Employee JsonToObj = emp_obj.fromJson(ObjTojson, Employee.class);
-            System.out.println(JsonToObj.name);
-            System.out.println(JsonToObj.designation);
-            System.out.println(JsonToObj.id);
-            System.out.println(JsonToObj.salary);
+            Employee[] JsonToObj = emp_obj.fromJson(ObjTojson, Employee[].class);
+            System.out.println(JsonToObj[2]);
         }
     }
